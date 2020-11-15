aaa96543e98a3493d4f0f534192e351155e3479d
Carlos Faria <carlos.faria@live.estacio.br>
2020-11-14 23:56:25 -0300

    -Criação da classe funcionário -A classe professor herda de funcionário -Implantação dos métodos para exibir os dados do funcionario

--- Aluno/funcionario.rb
+++ Aluno/funcionario.rb
@@ -1,17 +1,22 @@
-class Professor < Funcionario
+class Funcionario
 
     #o atributo ferias está definido como leitura
-    attr_accessor :disciplina
+    attr_reader :ferias, :data_inicio_ferias, :data_fim_ferias
+    attr_accessor :nome, :cod_func, :dt_nasc
     
     #Não é preciso inserir o atributo ferias para contruir um objeto
     #mas este é contruido automaticamente definido como FALSE
-    def initialize (nome, disciplina, cod_func, dt_nasc)
-        super(nome, cod_func, dt_nasc)
-        @disciplina = disciplina
+    def initialize (nome, cod_func, dt_nasc)
+        @nome = nome
+        @cod_func = cod_func
+        @dt_nasc = dt_nasc
+        @ferias = false
+        @data_inicio_ferias =  '00/00/00'
+        @data_fim_ferias = '00/00/00'
     end
 
     def imprime (funcionario)
-        puts "Cod.Funcionário: #{funcionario.cod_func} Nome: #{funcionario.nome} Disciplina: #{funcionario.disciplina}"
+        puts "Cod.Funcionário: #{funcionario.cod_func} Nome: #{funcionario.nome} Nascimento: #{funcionario.dt_nasc}"
     end
 
     # o método inicia as férias do professor
--- Aluno/turma.rb
+++ Aluno/turma.rb
@@ -1,5 +1,7 @@
 class Turma
-    attr_accessor :alunos, :nome, :num_sala, :professor
+    #define o atributo aluno como somente leitura
+    attr_reader :alunos
+    attr_accessor :nome, :num_sala, :professor
     
     def initialize (nome, num_sala, professor)
         @nome = nome
@@ -11,6 +13,10 @@ class Turma
     def adiciona_aluno(aluno)
         alunos << aluno
     end
+    #Esse trecho garante que os alunos somente serão inseridos
+    #na turma através do metodo adiciona_aluno
+    private
+    attr_writer :alunos
 
     def total_alunos()
         puts "Esta turma possui #{alunos.length.to_s()} alunos"
